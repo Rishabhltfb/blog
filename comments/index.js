e8bb3cefa1347f83184308f6ca9bf08899c5a153
Rishabhltfb <rishabhsharma.rs0403@gmail.com>
2021-07-25 23:32:03 +0530

    feat: add moderation service and evd events storage for service downtime handling

--- comments/index.js
+++ comments/index.js
@@ -10,15 +10,15 @@ app.use(cors());
 
 const commentsByPostId = {};
 
-app.get('/posts/:id/comments', ( req, res ) => {
-    res.send(commentsByPostId[req.params.id]||[]);
-});
+// app.get('/posts/:id/comments', ( req, res ) => {
+//     res.send(commentsByPostId[req.params.id]||[]);
+// });
 
 app.post('/posts/:id/comments',async (req,res) => {
     const commentId = randomBytes(4).toString('hex');
     const { content } = req.body;
     const comments = commentsByPostId[req.params.id] || [];
-    comments.push({id: commentId, content});
+    comments.push({id: commentId, content, status: 'pending'});
     commentsByPostId[req.params.id] = comments;
 
     await axios.post('http://localhost:4005/events',{
@@ -26,7 +26,8 @@ app.post('/posts/:id/comments',async (req,res) => {
         data: {
             id: commentId,
             content,
-            postId: req.params.id
+            postId: req.params.id,
+            status: 'pending'
         }
     });
 
@@ -34,9 +35,27 @@ app.post('/posts/:id/comments',async (req,res) => {
 });
 
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
     console.log('Received Event', req.body.type);
+    const { type, data } = req.body;
+    if(type === 'CommentModerated'){
+        const { postId, id, status } = data;
+        const comments = commentsByPostId[postId];
+        const comment = comments.find(comment => {
+            return comment.id === id;
+        });
+        comment.status = status;
 
+        await axios.post('http://localhost:4005/events',{
+            type: 'CommentUpdated',
+            data: {
+                id,
+                postId,
+                comment,
+                status
+            }
+        });
+    }
     res.send({});
 });
 
